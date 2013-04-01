be55bd6738f4d2f9acc84148e245d39759365d6e
Joshua French <jfrench@mines.edu>
2013-03-31 23:13:09 -0600

    Project 5
    - Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicStream::Application.config.secret_token = '3f6e08ab7cd8c04e640e811f308998a410a342d283e19f83cffb2be13346ff9995169e404d4227e4726e39f764d34f13ef8e9a30403433ecdbf08161666e5c47'
