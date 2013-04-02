77d9c364ba5081cd9216887b0f5753b3d80df970
Joshua French <jfrench@mines.edu>
2013-04-01 23:12:47 -0600

    Project 5
    - Updated Admin tests
    - Updated Session tests

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
   test "should get index" do
+  	dave = users(:dave)
+  	session[:user_id] = dave.id
     get :index
     assert_response :success
+    assert_not_nil assigns(@user)
   end
 
 end
