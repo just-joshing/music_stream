dcd4c76845e557fb9dec7efef6b604b0bc153333
Joshua French <jfrench@mines.edu>
2013-04-04 16:50:22 -0600

    Project 6
    - Added roles to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def authorize
-    unless User.find_by_id(session[:user_id])
+  	user = User.find_by_id(session[:user_id])
+    unless user and user.role == 'admin'
       redirect_to login_url, notice: 'Please log in'
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130402075341) do
+ActiveRecord::Schema.define(:version => 20130404223308) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "password_digest"
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
+    t.datetime "created_at",                              :null => false
+    t.datetime "updated_at",                              :null => false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "role",                :default => "user"
   end
 
 end
