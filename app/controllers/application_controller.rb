c1a034dd69008b7fedf946ba6cc4b2432358cd1a
Joshua French <jfrench@mines.edu>
2013-04-05 02:48:48 -0600

    Project 6
    - Moved notices and flash alerts from individual views to the common application layout
    - Admin pages enforce before filter establishing roles of user vs admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   def authorize_admin
   	user = User.find_by_id(session[:user_id])
     unless user and user.role == 'admin'
-      redirect_to login_url, notice: 'Please log in'
+      redirect_to :back
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -66,7 +66,7 @@ MusicStream::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#show'
+  root :to => 'sessions#new'
 
   # See how all your routes lay out with "rake routes"
 
