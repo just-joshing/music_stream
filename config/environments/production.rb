4a6353fe218b132865fc2b5116677e1c3b982bd5
Joshua French <jfrench@mines.edu>
2013-04-19 10:29:53 -0600

    Project 7
    - Trying precompile again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ MusicStream::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( welcome.js.erb )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -64,7 +64,4 @@ MusicStream::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  # Precompile page specific assets
-  config.assets.precompile += [ "welcome.js.erb", "users.js.erb", "sessions.js.erb", "admin.js.erb" ]
 end
