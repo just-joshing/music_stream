f75f4a36bab558e39c4d621e27a5ea40637e9a8d
Joshua French <jfrench@mines.edu>
2013-04-19 09:57:56 -0600

    Project 7
    - Added some files to be precompiled in production for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ MusicStream::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Precompile page specific assets
+  config.assets.preconfile += [ "welcome.js.erb", "users.js.erb", "sessions.js.erb", "admin.js.erb" ]
 end
